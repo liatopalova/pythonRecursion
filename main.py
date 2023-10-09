c158cda4751f7e41a1502e8adb20f90173e51fe2
Lia Topalova <lia.topalova@gmail.com>
2023-10-09 14:37:47 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+# ступінь числа
+def get_num_pow(number, degree):
+    if degree <= 1:
+        return number
+
+    return number * get_num_pow(number, degree - 1)
+
+
+print(get_num_pow(2, 3))
+
+# get_num_pow(2, 3) -> 2 * get_num_pow(2, 2) => 8
+# get_num_pow(2, 2) -> 2 * get_num_pow(2, 1) => 4
+# get_num_pow(2, 1) => 2
+
